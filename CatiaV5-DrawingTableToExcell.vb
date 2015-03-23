d717fe19e5f1af76249b60b06f663178e68841aa
Krzysztof Górzyński <gorzynskikrzysztof@gmail.com>
2015-03-23 16:56:36 +0100

    Update CatiaV5-DrawingTableToExcell.vb

--- CatiaV5-DrawingTableToExcell.vb
+++ CatiaV5-DrawingTableToExcell.vb
@@ -1,3 +1,25 @@
+'Copyright (c) 2014-2015 Krzysztof Gorzynski <gorzynskikrzysztof@gmail.com>
+'
+'Permission to use, copy, modify, and distribute this software for any
+'purpose with or without fee is hereby granted, provided that the above
+'copyright notice and this permission notice appear in all copies.
+'
+'THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+'WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+'MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+'ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+'WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+'ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+'OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+'----------------------------------------------------------------------------
+' Macro:    CatiaV5-DrawingTableToExcell.catvbs
+' Version:  0.1
+' Code:     Catia VBS
+' Purpose:  Convert CATDrawing Table into Excel Spreadsheet 
+' Autor:    Krzysztof Górzyński
+' Datum:    10/07/2014
+'----------------------------------------------------------------------------
+
 Sub CATMain()
 On Error Resume Next
 Set drawingDocument1 = CATIA.ActiveDocument
